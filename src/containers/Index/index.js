55da6e86bab73820cdb6f2ee3f839ed500b161b8
zhumin <zhumin@lianjia.com>
2017-12-26 19:37:39 +0800

    add 新增css-module支持

--- src/containers/Index/index.js
+++ src/containers/Index/index.js
@@ -1,6 +1,9 @@
 import React, { PureComponent } from 'react'
-import './index.styl'
+// import CSSModules from 'react-css-modules'
+// import style from './index.styl'
+import style from './index.css'
 
+// @CSSModules(style)
 export default class extends PureComponent {
   constructor(props) {
     super(props)
@@ -8,13 +11,14 @@ export default class extends PureComponent {
       say: 'Hello',
     }
   }
-
+  componentDidMount(){
+  }
   render() {
     const { state, props } = this
     console.log('State:', state, props)
     return (<section>
       { this.state.say }
-      <div className='test'>这里是一个方块</div>
+      <div className={style.title}>这里是一个方块</div>
     </section>)
   }
 }
